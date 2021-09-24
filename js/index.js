693a3a94f5df0b111ee365c66b2587dcd8bdd00d
OMAR <68195919+v5ln@users.noreply.github.com>
2021-09-25 01:47:17 +0300

    Update the code

--- js/index.js
+++ js/index.js
@@ -1,81 +1,76 @@
 class ProtocolHandler{
-    constructor(){
-        // if(ProtocolHandler.this == null){
-         this.connections = [];
-
-            // ProtocolHandler.instance = this;
-            
-        // }
-        // return ProtocolHandler.instance;
-    }
-
+    
     getInstance(connectionProtocol){
+        if (!ProtocolHandler.instance){
+            this.connections = [];
+            console.log('new instance');
+            ProtocolHandler.instance = this;
+        }
+
         if(this.connections.length == 3){
             console.log(this.connections[0] + " is disconnected");
             this.connections.shift();
         }
+
         this.release(connectionProtocol);
         this.connections.push(connectionProtocol)
+        return ProtocolHandler.instance;
     }
      
     send(message){
         console.log("----------");
         console.log(message);
     }
+
     getCurrentConnections(){
         console.log("----------");
         console.log("The Connections is :");
         console.log(this.connections);
-        // for(let i=0; i< this.connections.length; i++){
-        //     console.log((i+1)+": "+this.connections[i]);
-        // }
     }
+
     release(connectionProtocol){
         for(let i=0; i< this.connections.length; i++){
             if(this.connections[i] == connectionProtocol){
                 console.log(this.connections[i] + " is released");
                 this.connections.splice(i, 1);
             }
-    
         }
     }
 }
 
 const handler = new ProtocolHandler();
-Object.freeze(handler);
-handler;
-
 
 function ssh(){
-    handler.getInstance('ssh')
-    handler.send('ssh is connected');
-    handler.getCurrentConnections();
+    const ssh = handler.getInstance('ssh')
+    ssh.send('ssh is connected');
+    ssh.getCurrentConnections();
 }
 
 function telnet(){
-    handler.getInstance('telnet')
-    handler.send('telnet is connected');
-    handler.getCurrentConnections();
+    const telnet = handler.getInstance('telnet')
+    telnet.send('telnet is connected');
+    telnet.getCurrentConnections();
 }
+
 function http(){
-    handler.getInstance('http')
-    handler.send('http is connected');
-    handler.getCurrentConnections();
+    const http = handler.getInstance('http')
+    http.send('http is connected');
+    http.getCurrentConnections();
 }
+
 function scp(){
-    handler.getInstance('scp')
-    handler.send('scp is connected');
-    handler.getCurrentConnections();
-    handler.release('scp');
-    handler.getCurrentConnections();
+    const scp = handler.getInstance('scp')
+    scp.send('scp is connected');
+    scp.getCurrentConnections();
+    scp.release('scp');
+    scp.getCurrentConnections();
 }
+
 function ftp(){
-    handler.getInstance('ftp')
-    handler.send('ftp is connected');
-    handler.getCurrentConnections();
+    const ftp = handler.getInstance('ftp')
+    ftp.send('ftp is connected');
+    ftp.getCurrentConnections();
 }
-
-
 
 ssh();
 telnet();
